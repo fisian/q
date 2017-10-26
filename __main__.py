ae5a79e61e99246bdaebc4992c0d7609138fbe45
Andreas Fischer <andreas.n.fischer@gmail.com>
2017-10-26 14:20:33 +0200

    Implement blocks of executable code
    Blocks are started by specifying a block id and the action begin block (q q q)
    Then the block code follows and then the block is closed by using the same block id and the end block action (q q qq)
    Blocks can be called with the run local block action (q qq)
    
    The code.qqq file has an example for a simple usage of blocks

--- __main__.py
+++ __main__.py
@@ -2,24 +2,36 @@
 
 import re
 import types
-import operator
 import sys
 
-debug = False
-
+debuglevels = {"trace": 0, "debug": 1, "none": 8}
 
 class MachineState:
-    def __init__(self):
+    def __init__(self, analyze = False):
+        #debug flag
+        self.debug = 8
         # stack to store operators
         self.stack = []
         # set of possible interpreter states
-        self.states = {"action": 0, "type": 1, "value": 2}
+        self.states = {"action": 0, "type": 1, "value": 2, "block": 3}
         # current internal state/next expected value
         self.state = self.states["type"]
+        # block storage to store loaded blocks
+        self.blocks = {}
+        # current block identifier
+        self.block = ""
+        # if set to True token evaluation does only change states but does not execute actions (used for block evaluation)
+        self.analyze = analyze
         # action resolving
         self.actions = {
+                # Reserved namespace
+                "q": {
+                    # Special actions
+                    "q": {"q": self.beginBlock, "qq": self.endBlock},
+                    # Locally defined blocks
+                    "qq": self.runLocalBlock},
                 # String functions
-                "qq": {"q": self.printNextStack},
+                "qq": {"q": self.printNextStack, "qq": self.printNl},
                 # Number functions
                 "qqq": {"q": self.addNumbers}}
         # current action namespace resolution
@@ -34,43 +46,68 @@ class MachineState:
         self.sChars = "  !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~\n"
         self.currentType = self.types["eval"]
 
+    def resetState(self):
+        self.state = self.states["type"]
+
+    def evalProgram(self, program):
+        for token in program:
+            self.eval(token)
+
     # evaluate the current token in respect to state and action stack
     def eval(self, token):
         if self.state is self.states["action"]:
-            self.state = self.states["action"]
             if type(self.action) is dict:
                 if token in self.action:
                     self.action = self.action[token]
+                    self.setState("action")
                 else:
-                    self.state = self.states["type"]
+                    self.setState("type")
+                    self.action = self.actions
                     print("ACTION ERROR: Action could not be resolved!")
             if type(self.action) is types.MethodType:
-                self.action()
-                self.action = self.actions
-                self.state = self.states["type"]
-                if debug:
+                self.setState("type")
+                if not self.analyze:
+                    self.action()
+                if self.debug <= 1:
                     print("EXECUTING: " + str(self.actions[token]))
-
 
         elif self.state is self.states["type"]:
             if token is self.types["eval"]:
-                self.state = self.states["action"]
+                if self.debug <= debuglevels["trace"]:
+                    print("TYPE: eval")
+                self.startAction()
             else:
                 if token in self.types.values():
-                    self.currentType = token
-                    if debug:
-                        print("TYPE: " + str(list(self.types.keys())[list(self.types.values()).index(self.currentType)]))
+                    self.stack.append([token,None])
+                    if self.debug <= debuglevels["trace"]:
+                        print("TYPE: " + str(list(self.types.keys())[list(self.types.values()).index(self.currentType)]) + " (" + token + ")")
+                    self.setState("value")
                 else:
                     print("TYPE ERROR: " + str(token))
 
-                self.state = self.states["value"]
-
         elif self.state is self.states["value"]:
-            self.stack.append((self.currentType, token))
-            if debug:
+            self.stack[-1][1] = token
+            self.setState("type")
+            if self.debug <= debuglevels["trace"]:
                 print("PARAM: " + str(token))
 
-            self.state = self.states["type"]
+        elif self.state is self.states["block"]:
+            if self.debug <= debuglevels["trace"]:
+                print("BLOCK " + self.block + " ADD TOKEN: " + token)
+            blk = self.blocks[self.block]
+            blkState = blk[0]
+            blkProgram = blk[1]
+            blkProgram.append(token)
+            blkState.eval(token)
+            if blkState.isActionEndBlock() == self.block:
+                if self.debug <= debuglevels["debug"]:
+                    print("BLOCK " + self.block + " END")
+                blkState.resetState()
+                blkState.analyze = False
+                blkState.stack = self.stack
+                blkState.blocks = self.blocks
+                self.setState("type")
+                self.block = ""
 
         else:
             print("INTERNAL STATE ERROR: " + str(self.state))
@@ -93,10 +130,20 @@ class MachineState:
         else:
             print("INTERNAL ACTION ERROR: " + str(paramType) + " : " + str(list(self.types.keys())[list(self.types.values()).index(paramType)]))
 
-        if debug:
+        if self.debug <= debuglevels["trace"]:
             print("RESOLVED PARAM: " + str(resolvedParam))
 
         return resolvedParam
+
+    # helper method to set machine state
+    def setState(self, state):
+        if self.debug <= debuglevels["trace"]:
+            print("STATE " + state)
+        self.state = self.states[state]
+
+    def startAction(self):
+        self.setState("action")
+        self.action = self.actions
 
     # helper method for getting the topmost stack item
     def popStack(self):
@@ -105,11 +152,55 @@ class MachineState:
         else:
             print("PARAMETER ERROR: No parameters on stack!")
 
+    # method to start building a block
+    def beginBlock(self):
+        num = self.stack.pop()
+        if num[0] == self.types["+Number"]:
+            if self.debug <= debuglevels["debug"]:
+                print("Make block " + num[1])
+            self.setState("block")
+            self.block = num[1]
+            blkMachine = MachineState(analyze = True)
+            self.blocks[num[1]] = (blkMachine, [])
+        else:
+            print("BLOCK ERROR: Disallowed block index type: " + str(num[0]))
+
+    # method to end building a block
+    # this only represents the token at the end of a block
+    # should never be evaluated
+    def endBlock(self):
+        self.popStack() # Remove specified block id
+        if self.debug <= debuglevels["debug"]:
+            print("BLOCK END")
+
+    # check if current action is 
+    def isActionEndBlock(self):
+        if self.action == self.endBlock:
+            blockId = self.stack.pop()
+            if blockId[0] == self.types["+Number"]:
+                return blockId[1]
+        return None
+
+    def runLocalBlock(self):
+        num = self.stack.pop()
+        if num[0] == self.types["+Number"]:
+            if self.debug <= debuglevels["debug"]:
+                print("Run local block " + num[1])
+            if num[1] in self.blocks:
+                blk = self.blocks[num[1]]
+                blk[0].evalProgram(blk[1])
+        else:
+            print("BLOCK ERROR: Disallowed block index type: " + str(num[0]))
+
     # prints the topmost element from stack
     def printNextStack(self):
         print(self.popStack(), end="")
 
-    # adds the two topmost elemenst from stack
+    # prints newline
+    def printNl(self):
+        print()
+
+    # adds the two topmost element from stack
     def addNumbers(self):
         num1 = self.stack.pop()
         num2 = self.stack.pop()
@@ -131,11 +222,11 @@ def stringToProgram(code):
     return code.split()
 
 machine = MachineState()
+machine.debug = 8
 if len(sys.argv) > 1:
-    code = str(sys.argv)
+    code = str(sys.argv[1:])
     program = stringToProgram(code)
-    for token in program:
-        machine.eval(token)
+    machine.evalProgram(program)
 else:
     print("q-REPL. Type 'quit' to exit.")
     while True:
@@ -144,5 +235,4 @@ else:
             print("Exiting")
             sys.exit(0)
         program = stringToProgram(code)
-        for token in program:
-            machine.eval(token)
+        machine.evalProgram(program)
